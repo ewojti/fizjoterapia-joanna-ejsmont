a55ca06caf9bedcd94ea3af00df2b05cd0e3ff30
Wojtek E <wojejs@gmail.com>
2023-02-17 10:13:12 +0100

    add first styling to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Navbar.scss'
+import { Link } from "react-router-dom";
+import images from '../../constants/images'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className='app__navbar'>
+      <div className="app__navbar-logo">
+        <img src={images.logo_header} alt="navbar logo fizjoterapia" />
+      </div>
+      <div className='app__navbar-links'>
+        <Link to="">O mnie</Link>
+        <Link to="">Cennik</Link>
+        <Link to="">Usługi</Link>
+        <Link to="">Blog</Link>
+        <Link to="">Kontakt</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import aboutme from "../assets/aboutme.jpg";
 import header from "../assets/header.jpg";
 import stopy from "../assets/stopy.jpg";
+import logo_header from "../assets/logo_header.jpg";
+import logo_faq from "../assets/logo_faq.png";
 
 export default {
   aboutme,
   header,
   stopy,
+  logo_header,
+  logo_faq,
 };
