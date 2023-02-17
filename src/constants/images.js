1eddb9b1d0d365fe1bec58b826f0e14527d7aa8b
Wojtek E <wojejs@gmail.com>
2023-02-17 08:36:33 +0100

    moving global changes from index.scss to app.scss

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import aboutme from "../assets/aboutme.jpg";
+import header from "../assets/header.jpg";
+import stopy from "../assets/stopy.jpg";
+
+export default {
+  aboutme,
+  header,
+  stopy,
+};
