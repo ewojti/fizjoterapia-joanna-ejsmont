edcefddab6dba244c5c729c3f901185a14a94980
Wojtek E <wojejs@gmail.com>
2023-02-18 17:20:05 +0100

    add shadows to img

--- src/pages/PricingDetails.js
+++ src/pages/PricingDetails.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PriceList from '../container/PriceList/PriceList';
+// import Navbar from '../../components/Navbar/Navbar'
+// import Footer from '../../components/Footer/Footer'
 
 const PricingDetails = () => {
   return (
     <div>
-        <PriceList />
+      {/* <Navbar/>  */}
+      <PriceList />
+      {/* <Footer /> */}
     </div>
   )
 }
