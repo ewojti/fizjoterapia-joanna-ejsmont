806dc7a913ac226854dc0a70415ccee7020726b2
Wojtek E <wojejs@gmail.com>
2023-02-18 13:33:36 +0100

    add desktop styles and content to services section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <div>
+        Icons made by
+        <a href="https://www.freepik.com" title="Freepik">
+          Freepik
+        </a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com'
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
--- src/container/Services/Services.js
+++ src/container/Services/Services.js
@@ -5,25 +5,26 @@ import './Services.scss'
 const Services = () => {
   return (
     <div className="app__services section__padding">
+      <div className="app__border-section"></div>
       <h1 className="app__heading-h1">W czym mogę Ci pomóc?</h1>
       <div className="app__services-list">
-      {services.map((service) => (
-        <div className="app__services-list-item" key={service.id}>
-          <h2 className="app__heading-h2">{service.title}</h2>
-          <div className="app__service-type">
+        {services.map((service) => (
+          <div className="app__services-list-item" key={service.id}>
             <img
               className="app__service-icon"
               src={service.icon}
               alt="fizjoterapia rehabilitacja"
-              />
-            <ul>
-              {service.types.map((type) => (
-                <li>{type}</li>
+            />
+            <div className="app__service-list-item-description">
+              <h2 className="app__heading-h2">{service.title}</h2>
+              <ul>
+                {service.types.map((type) => (
+                  <li>{type}</li>
                 ))}
-            </ul>
+              </ul>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
