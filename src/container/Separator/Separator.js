48333b897cf4d448ab0e7c410e0d5a52cf38e88c
Wojtek E <wojejs@gmail.com>
2023-02-19 09:32:59 +0100

    add separator component

---
+++ src/container/Separator/Separator.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Separator = () => {
+  return <div className="app__bg-separator"></div>;
+}
+
+export default Separator
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
 import Header from '../container/Header/Header'
 import AboutMe from '../container/AboutMe/AboutMe'
+import Separator from '../container/Separator/Separator'
 import Blog from '../container/Blog/Blog'
 import Services from '../container/Services/Services'
 import Contact from '../container/Contact/Contact'
@@ -12,6 +12,7 @@ const Home = () => {
       <Header />
       <Services />
       <AboutMe />
+      <Separator />
       <Blog />
       <Contact />
     </div>
