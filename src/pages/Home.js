76d746a634e1c0f49bc35b79fedc519364bdebcc
Wojtek E <wojejs@gmail.com>
2023-02-21 00:58:09 +0100

    restore hidden components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,9 +12,9 @@ const Home = () => {
       <Header />
       <Services />
       <AboutMe />
-      {/* <Separator />
+      <Separator />
       <Blog />
-      <Contact /> */}
+      <Contact />
     </div>
   );
 }
