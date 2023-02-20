cb1c3c3f5a6fe04ee4fd422628f67804444db2b1
Wojtek E <wojejs@gmail.com>
2023-02-21 00:56:34 +0100

    add responsive view to aboutme component

--- src/container/AboutMe/AboutMe.js
+++ src/container/AboutMe/AboutMe.js
@@ -5,29 +5,35 @@ import images from '../../constants/images'
 const AboutMe = () => {
   return (
     <div className="app__aboutme section__padding">
-      <div className="app__aboutme-description">
-        <h2 className="app__heading-h2">Kim jestem?</h2>
-        <p className="p__inter">
-          Proin sagittis sapien quam, nec laoreet justo varius at. Donec sed dui
-          arcu. Vivamus commodo cursus risus. Suspendisse nec dictum dolor, vel
-          venenatis odio. Sed pellentesque, ligula vel tempus gravida, felis
-          tortor tristique est, et mattis lectus mi at lorem.
-          <br />
-          <br />
-          Proin malesuada tincidunt lacinia. Morbi id dui velit. Ut id turpis eu
-          leo efficitur rhoncus eu a odio. Phasellus scelerisque a erat in
-          vehicula. Praesent convallis porta nisl, ac fringilla velit posuere a.
-          <br />
-          <br />
-          Donec a nunc idarcu porttitor varius et a lacus. Nulla quis porttitor
-          nisl. Nunc ut eros finibus, vehicula ex ac, ultricies nunc. Sed ornare
-          sit amet mi ut sagittis. Nullam quis laoreet dui. Fusce lobortis
-          interdum eleifend.
-        </p>
-        <button className="custom__button">Rozwiń</button>
-      </div>
       <div className="app__aboutme-img">
         <img src={images.aboutme} alt="" />
+      </div>
+      <div className="app__aboutme-description">
+        <div className="app__aboutme-description-text">
+          <h2 className="app__heading-h2">Kim jestem?</h2>
+          <p className="p__lato">
+            Proin sagittis sapien quam, nec laoreet justo varius at. Donec sed
+            dui arcu. Vivamus commodo cursus risus. Suspendisse nec dictum
+            dolor, vel venenatis odio.
+            <br />
+            <br />
+            Proin malesuada tincidunt lacinia. Morbi id dui velit. Ut id turpis
+            eu leo efficitur rhoncus eu a odio. Phasellus scelerisque a erat in
+            vehicula. Praesent convallis porta nisl, ac fringilla velit posuere
+            a.
+            <br />
+            <br />
+            Donec a nunc idarcu porttitor varius et a lacus. Nulla quis
+            porttitor nisl. Nunc ut eros finibus, vehicula ex ac, ultricies
+            nunc. Sed ornare sit amet mi ut sagittis. Nullam quis laoreet dui.
+            Fusce lobortis interdum eleifend.Phasellus scelerisque a erat in
+            vehicula. Praesent convallis porta nisl, ac fringilla velit posuere
+            a.
+          </p>
+        </div>
+        <div className="app__aboutme-description-btn">
+          <button className="custom__button">Rozwiń</button>
+        </div>
       </div>
     </div>
   );
