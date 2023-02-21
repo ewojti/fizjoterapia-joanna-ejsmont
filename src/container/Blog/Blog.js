c600fd32c73d12f278b04f6c342e68f928b9a98e
Wojtek E <wojejs@gmail.com>
2023-02-21 12:20:46 +0100

    add responsive view to blog component

--- src/container/Blog/Blog.js
+++ src/container/Blog/Blog.js
@@ -5,23 +5,28 @@ import blogData from "../../constants/blogData";
 const Blog = () => {
   return (
     <div className="app__blog section__padding">
-      <h1 className="app__heading-h1">Mój Blog</h1>
+      <div className="app__blog-title">
+        <h1 className="app__heading-h1">Mój Blog</h1>
+        <button className="app__blog-button p__inter">
+          Zobacz wszystkie posty
+        </button>
+      </div>
       <div className="app__blog-posts">
         {blogData.map((post) => (
           <div className="app__blog-post">
             <div className="app__blog-post-text">
-              <h3>{post.title}</h3>
+              <div className="app__blog-post-date">
+                <p>{post.date}</p>
+              </div>
+              <h3 className="app__heading-h3">{post.title}</h3>
               <p>{post.description}</p>
             </div>
-            <div className="app__blog-post-date">
-              <p>{post.date}</p>
+            <div className="app__blog-post-read">
+              <a href="blank">Przeczytaj</a>
             </div>
           </div>
         ))}
       </div>
-      <button className="app__blog-button p__inter">
-        Zobacz wszystkie posty
-      </button>
     </div>
   );
 };
